33211d83c376469b0ce518dc29464317dec7604e
Abdurahmon007 <elbekmovlonov@gmail.com>
2023-11-24 18:59:32 +0500

    Forms added

--- DG-client/src/features/users/EditUserForm.jsx
+++ DG-client/src/features/users/EditUserForm.jsx
@@ -1,16 +1,25 @@
-import { useState, useEffect } from "react";
-import { useAddNewUserMutation } from "./usersApiSlice";
+import React, { useEffect, useState } from "react";
+import { useDeleteUserMutation, useUpdateUserMutation } from "./usersApiSlice";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSave } from "@fortawesome/free-solid-svg-icons";
+import { faSave, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { ROLES } from "../../config/roles";
 
 const USER_REGEX = /^[A-z]{3, 20}$/;
 const PWD_REGEX = /^[A-z0-9!@#$%]{4,12}$/;
 
-function NewUserForm() {
-  const [addNewUser, { isLoading, isError, isSuccess, error }] =
-    useAddNewUserMutation();
+function EditUserForm({ user }) {
+  const [updateUser, { isLoading, isSuccess, isError, error }] =
+    useUpdateUserMutation();
+  const [
+    deleteUser,
+    {
+      isLoading: isDelLoading,
+      isSuccess: isDelSuccess,
+      isError: isDelError,
+      error: delError,
+    },
+  ] = useDeleteUserMutation();
 
   const navigate = useNavigate();
 
@@ -18,7 +27,8 @@ function NewUserForm() {
   const [validUsername, setValidUsername] = useState(false);
   const [password, setPassword] = useState("");
   const [validPassword, setValidPassword] = useState(false);
-  const [roles, setRoles] = useState(["Employee"]);
+  const [roles, setRoles] = useState(user.roles);
+  const [active, setActive] = useState(user.active);
 
   useEffect(() => {
     setValidUsername(USER_REGEX.test(username));
@@ -29,7 +39,7 @@ function NewUserForm() {
   }, [password]);
 
   useEffect(() => {
-    if (isSuccess) {
+    if (isSuccess || isDelSuccess) {
       setUsername("");
       setPassword("");
       setRoles([""]);
@@ -49,14 +59,18 @@ function NewUserForm() {
     setRoles(values);
   };
 
-  const canSave =
-    [roles.length, validUsername, validPassword].every(Boolean) && !isLoading;
+  const onActiveChanged = () => setActive((prev) => !prev);
 
   const onSaveUserClicked = async (e) => {
-    e.preventDefault();
-    if (canSave) {
-      await addNewUser({ username, password, roles });
+    if (password) {
+      await updateUser({ id: user.id, username, password, roles, active });
+    } else {
+      await updateUser({ id: user.id, username, roles, active });
     }
+  };
+
+  const onDeleteUser = async (e) => {
+    await deleteUser({ id: user.id });
   };
 
   const options = Object.values(ROLES).map((role) => {
@@ -67,21 +81,45 @@ function NewUserForm() {
     );
   });
 
-  const errClass = isError ? "errmsg" : "offscreen";
+  let canSave;
+
+  if (password) {
+    canSave =
+      [roles.length, validUsername, validPassword].every(Boolean) && !isLoading;
+  } else {
+    canSave = [roles.length, validUsername].every(Boolean) && !isLoading;
+  }
+
+  const errClass = isError || isDelError ? "errmsg" : "offscreen";
   const validUserClass = !validUsername ? "form__input--incomplete" : "";
-  const validPwdClass = !validPassword ? "form__input--incomplete" : "";
+  const validPwdClass =
+    password && !validPassword ? "form__input--incomplete" : "";
   const validRolesClass = !Boolean(roles.length)
     ? "form__input--incomplete"
     : "";
 
+  const errContent = (error?.data?.message || error?.data?.message) ?? "";
+
   const content = (
     <>
-      <p className={errClass}>{error?.data?.message}</p>
+      <p className={errClass}>{errContent}</p>
       <form className="form" onSubmit={onSaveUserClicked}>
         <div className="form__title-row">
-          <h2>New User</h2>
+          <h2>Edit User</h2>
           <div className="form__action-buttons">
-            <button className="icon-button" title="Save" disabled={!canSave}>
+            <button
+              className="icon-button"
+              title="Save"
+              onClick={onSaveUserClicked}
+              disabled={!canSave}
+            >
+              <FontAwesomeIcon icon={faSave} />
+            </button>
+            <button
+              className="icon-button"
+              title="Delete"
+              onClick={onDeleteUser}
+            >
               <FontAwesomeIcon icon={faSave} />
             </button>
           </div>
@@ -112,6 +150,21 @@ function NewUserForm() {
           onChange={onPasswordChanged}
         />
 
+        <label
+          className="form__label form__checkbox-container"
+          htmlFor="user-active"
+        >
+          ACTIVE:
+          <input
+            className="form__checkbox"
+            id="user-active"
+            name="user-active"
+            type="checkbox"
+            checked={active}
+            onChange={onActiveChanged}
+          />
+        </label>
+
         <label className="form__label" htmlFor="roles">
           Assigned Roles:
         </label>
@@ -134,4 +187,4 @@ function NewUserForm() {
   return content;
 }
 
-export default NewUserForm;
+export default EditUserForm;
